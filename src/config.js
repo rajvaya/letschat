cf89871481ef2521f9cbff06f8cb073e3bae5a10
raj vaya <rajkumarvaya@gmail.com>
2021-07-15 10:32:11 +0530

    🎉 Lets Chat init

---
+++ src/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBuQe9WsW74rn4lWSGF9zBhbl2ySmG9_pg",
+  authDomain: "letschat-1111.firebaseapp.com",
+  databaseURL: "https://letschat-1111-default-rtdb.firebaseio.com",
+  projectId: "letschat-1111",
+  storageBucket: "letschat-1111.appspot.com",
+  messagingSenderId: "5275177404",
+  appId: "1:5275177404:web:f81a162ec4c3b0647f1bb9",
+};
